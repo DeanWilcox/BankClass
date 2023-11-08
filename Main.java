e6ed2ed95415fca303efb588853db70b2eb88e75
dwilcox <wilcox.dean93@gmail.com>
2023-11-08 14:58:46 -0800

    many changes

--- Main.java
+++ Main.java
@@ -1,14 +1,30 @@
+//import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Bank bank1 = new Bank();
-        //Bank bank2 = new Bank();
-        bank1.Deposit(500);
-        bank1.Deposit(50);
-        bank1.Deposit(10);
-        double currentBalance = bank1.GetBalance();
-        System.out.println(currentBalance);
-        bank1.withdrawl(5);
-        double balanceAfterWithdrawl = bank1.GetBalance();
-        System.out.println(balanceAfterWithdrawl);
+        Bank bank1 = new Bank("Melissa", 900);
+        System.out.println(bank1.GetBalance());
+        System.out.println(bank1.GetBankBalance());
+        Bank bank2 = new Bank("Eric", 500);
+        System.out.println(bank2.GetBalance());
+        System.out.println(bank2.GetBankBalance());
+        Bank bank3 = new Bank("Peter", 1000);
+        System.out.println(bank3.GetBalance());
+        System.out.println(bank3.GetBankBalance());
+
+
+        System.out.println(bank1.GetBankBalance());
+        // System.out.println(bank1.AuthorizedUser);
+        // System.out.println(bank1.GetBalance());
+        // System.out.println(bank1.AccountType);
+        // bank1.Deposit(500);
+        // bank1.Deposit(50);
+        // bank1.Deposit(10);
+        // double currentBalance = bank1.GetBalance();
+        // System.out.println(currentBalance);
+        // bank1.Withdrawal(5);
+        // currentBalance = bank1.GetBalance();
+        // System.out.println(currentBalance);
+        // bank1.Balance = 1000000000;
     }
 }
